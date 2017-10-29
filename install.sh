9c2ef971ddd150f12495523551cb035e7739acef
Adam <acoggesh@uwyo.edu>
2017-10-29 00:18:51 +0000

    Add default args.

--- install.sh
+++ install.sh
@@ -3,4 +3,7 @@ sudo apt-get install lib32gcc1
 sudo apt-get install lib32stdc++6
 wget -qO- "https://steamcdn-a.akamaihd.net/client/installer/steamcmd_linux.tar.gz" | tar -xzvf -
 echo "quit" | ./steamcmd.sh
-touch gmod_args.txt
+echo -e "+hostname \"My Server\"\n+map gm_construct\n+maxplayers 10" > gmod_args.txt
+
+# DO NOT RUN THE FOLLOWING UNLESS YOU WANT TO DELETE EVERYTHING
+# TO DELETE EVERYTHING: git clean -fdX
